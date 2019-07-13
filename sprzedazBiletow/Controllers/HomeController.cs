5c9474fdbe21345e020f7399d1a0d371b69c1279
Aleksandra Wilke <olawilke@wp.pl>
2019-07-13 17:50:38 +0200

    mocno okrojony szablon

---
+++ sprzedazBiletow/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sprzedazBiletow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*
+        public string Index(string index)
+        {
+            return "Witam na stronie!" + index;
+        }*/
+
+        public ActionResult Wyszukaj()
+        {
+            ViewBag.Message = ":)";
+
+            return View();
+        }
+
+        public ActionResult Kup()
+        {
+            ViewBag.Message = ":)";
+
+            return View();
+        }
+    }
+}
---
+++ sprzedazBiletow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sprzedazBiletow.MvcApplication" Language="C#" %>
